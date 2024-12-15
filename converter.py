a178ed62d2006fa6c0fdfce0a517b210237f0e20
GregorSiefert <gregor.siefert@gmx.de>
2024-12-15 16:39:15 +0100

    einfügen von benötigtem delay zwischen Inputs und dem ersten Modus "Alt+F4"

--- converter.py
+++ converter.py
@@ -2,7 +2,8 @@ import sys
 
 indent =   "    "   #derzeitige einrückung
 name = "Keyboard."  #name der Keyboard Bibliothek
-n = ";\n"            #weil ich keine lust habe \ zu drücken
+needed_delay = 50   #delay needed to not skip inputs 
+n = ";\n"           #weil ich keine lust habe \ zu drücken
 def duden(wort):    #https://docs.arduino.cc/language-reference/de/en/functions/usb/Keyboard/keyboardModifiers/
     if len(wort)==1:
         return "\"" + wort + "\""
@@ -53,6 +54,54 @@ def duden(wort):    #https://docs.arduino.cc/language-reference/de/en/functions/
             return "KEY_ESC"
         case "pause":
             return "KEY_PAUSE"
+        case "F1":
+            return "KEY_F1"
+        case "F2":
+            return "KEY_F2"
+        case "F3":
+            return "KEY_F3"
+        case "F4":
+            return "KEY_F4"
+        case "F5":
+            return "KEY_F5"
+        case "F6":
+            return "KEY_F6"
+        case "F7":
+            return "KEY_F7"
+        case "F8":
+            return "KEY_F8"
+        case "F9":
+            return "KEY_F9"
+        case "F10":
+            return "KEY_F10"
+        case "F11":
+            return "KEY_F11"
+        case "F12":
+            return "KEY_F12"
+        case "F13":
+            return "KEY_F13"
+        case "F14":
+            return "KEY_F14"
+        case "F15":
+            return "KEY_F15"
+        case "F16":
+            return "KEY_F16"
+        case "F17":
+            return "KEY_F17"
+        case "F18":
+            return "KEY_F18"
+        case "F19":
+            return "KEY_F19"
+        case "F20":
+            return "KEY_F20"
+        case "F21":
+            return "KEY_F21"
+        case "F22":
+            return "KEY_F22"
+        case "F23":
+            return "KEY_F23"
+        case "F24":
+            return "KEY_F24"
         case _:
             print("duden hat nicht enthaltenes Wort erhalten: " + wort)
             sys.exit()
@@ -77,16 +126,17 @@ file_c.write("// Crackname: "+ txt[0] + "\n")
 file_c.write("void " + txt[2] + "_" + txt[1] + "() { \n")
 
 for index in range(3,len(txt),2):
-    print(txt[index])
+    print(txt[index]+": "+txt[index+1])
     match txt[index]:
         case "text":
             file_c.write(indent+name+"print(\""+txt[index+1]+"\")"+n)
         case "textl":
             file_c.write(indent+name+"println(\""+txt[index+1]+"\")"+n)
         case "comb":
-            comb_list = txt[index+1].split
+            comb_list = txt[index+1].split()
             for index2 in range (0,len(comb_list),1):
                 file_c.write(indent+name+"press("+duden(comb_list[index2])+")"+n)
+                file_c.write(indent+"delay("+str(needed_delay)+")"+n)
             file_c.write(indent+name+"releaseAll()"+n)
         case "stroke":
             file_c.write(indent+name+"write("+duden(txt[index+1])+")"+n)
@@ -108,6 +158,7 @@ for index in range(3,len(txt),2):
         case _:
             print("Befehl nicht erkannt: " + txt[index] + " zeile " + str(index))
             sys.exit()
+    file_c.write(indent+"delay("+str(needed_delay)+")"+n)
 
 if indent == "        ":
     file_c.write("    }" + n)
